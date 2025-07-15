1eaaa7cdeef7e843c442792228e2e3dc9c91803a
thecodingmontana <dev.humblechris254@gmail.com>
2025-07-15 11:33:38 +0300

    feat(app): add GraphQL ESLint Plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,53 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import { mergeProcessors } from 'eslint-merge-processors';
+import pluginVue from 'eslint-plugin-vue';
+import processorVueBlocks from 'eslint-processor-vue-blocks';
+import js from '@eslint/js';
+import graphqlPlugin from '@graphql-eslint/eslint-plugin';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default [
+  {
+    files: ['**/*.js'],
+    processor: graphqlPlugin.processor,
+    rules: js.configs.recommended.rules,
+  },
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    // Vue still needs to be parsed by the Vue parser for normal linting. But GraphQL's lint needs to lint only the JS/TS part,
+    // so extract those as blocks using eslint-processor-vue-blocks. This turns the script parts of Vue SFCs into virtual JS/TS
+    // blocks inside ESLint. ESLint can then parse the JS/TS to find GraphQL parts. And finally, graphql-eslint can lint the resulting GraphQL
+    processor: mergeProcessors([
+      pluginVue.processors.vue,
+      processorVueBlocks({
+        blocks: {
+          script: true,
+          scriptSetup: true,
+          customBlocks: true,
+        },
+      }),
+    ]),
+  },
+  {
+    files: ['**/*.graphql'],
+    languageOptions: {
+      parser: graphqlPlugin.parser,
+    },
+    plugins: {
+      '@graphql-eslint': graphqlPlugin,
+    },
+    rules: {
+      '@graphql-eslint/no-anonymous-operations': 'error',
+      '@graphql-eslint/no-duplicate-fields': 'error',
+      '@graphql-eslint/naming-convention': [
+        'error',
+        {
+          OperationDefinition: {
+            style: 'PascalCase',
+            forbiddenPrefixes: ['Query', 'Mutation', 'Subscription', 'Get'],
+            forbiddenSuffixes: ['Query', 'Mutation', 'Subscription'],
+          },
+        },
+      ],
+    },
+  },
+];
